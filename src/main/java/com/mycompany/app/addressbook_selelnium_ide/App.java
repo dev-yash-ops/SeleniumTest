f0fb7998ab357991a4e44a588e42745f3c3a6718
dev-yash-ops <80963616+dev-yash-ops@users.noreply.github.com>
2021-04-05 18:19:21 +0530

    Uploading Selenium Testcases App

---
+++ src/main/java/com/mycompany/app/addressbook_selelnium_ide/App.java
@@ -0,0 +1,43 @@
+package com.mycompany.app.addressbook_selelnium_ide;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test
+    public static void main( String[] args )
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\yash\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        
+        System.out.println("Welcome to Selenium Demo");
+        
+        driver.get("http://13.234.29.0:8889/");
+        
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        
+        
+         driver.findElement(By.className("v-button")).click();
+         driver.findElement(By.id("gwt-uid-5")).sendKeys("Yash Maurya");
+         driver.findElement(By.id("gwt-uid-7")).sendKeys("DevOps Engineer");
+         driver.findElement(By.id("gwt-uid-9")).sendKeys("7698931133");
+         driver.findElement(By.id("gwt-uid-11")).sendKeys("devyashops@gmail.com");
+         driver.findElement(By.id("gwt-uid-13")).sendKeys("04/18/1995");
+         driver.findElement(By.className("v-button-primary")).click();
+         //Thread.sleep(5000)
+         //driver.quit();
+         
+    }
+}
